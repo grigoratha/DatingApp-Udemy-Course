fae3fe08ed371697ba9dedca095c39a20e2febeb
grigoratha <grigoratha@aueb.gr>
2025-11-23 23:38:25 +0200

    Initial commit

---
+++ API/Program.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(options => options
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.MapControllers();
+
+app.Run();
---
+++ client/src/app/app.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+ 
+  private http = inject(HttpClient);
+  // Deprecated
+  //constructor(private http: HttpClient) {}
+
+  protected title = 'Dating App';
+  protected members = signal<any>([]);
+
+   async ngOnInit() {
+    /*
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => this.members.set(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the http request')
+    });
+    */
+
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch(error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  
+}
